1c8578b9e5720737f19a62b77fc8cdd68bbb2806
Donghyun An <miehdan@gmail.com>
2017-08-30 16:28:46 +0900

    Q290 - 73

--- Main.java
+++ Main.java
@@ -1,31 +1,22 @@
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.*;
 
 public class Main {
-    interface A { 
-        void x(); 
+    public class Hello {
+        String title;
+        int value;
+        public Hello() {
+            title += " World";
+        }
+        public Hello(int value) {
+            this.value = value;
+            title = "Hello";
+            Hello();
+        }
     }
-    class B implements A { 
-        public void x() {
-            System.out.println("B.x()");
-        } 
-        public void y() {
-            System.out.println("B.y()");
-        } 
-    }
-    class C extends B { 
-        public void x() {
-            System.out.println("C.x()");
-        } 
-    } 
     public void main(String[] args) {
-        java.util.List<A> list = new java.util.ArrayList<A>();
-        list.add(new B());
-        list.add(new C());
-        for (A a : list) {
-          a.x();
-          // a.y();
-      }
-  }
+        Hello c = new Hello(5);
+        System.out.println(c.title);
+    }
 }
+
+
