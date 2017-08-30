00b836aa84c3122c60bc2d638c7e5b2b10624d85
Donghyun An <miehdan@gmail.com>
2017-08-30 17:26:11 +0900

    Q290 - 75

---
+++ Client.java
@@ -0,0 +1,11 @@
+class Target {
+    private int i = 0;
+    public int addOne(){
+        return ++i;
+    }
+} 
+public class Client {
+    public static void main(String[] args){
+        System.out.println(new Target().addOne());
+    }
+}
