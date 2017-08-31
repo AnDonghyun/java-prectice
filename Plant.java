8140ee062e009fa6acf4c2772c0fc608efc7f104
Donghyun An <miehdan@gmail.com>
2017-08-31 18:01:07 +0900

    Q290 - 88

---
+++ Plant.java
@@ -0,0 +1,17 @@
+public class Plant {
+    private String name;
+    // public Plant() {
+    //     this("fern");
+    // }
+    public Plant(String name) {
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+}
+
+// public class Tree extends Plant {
+//     public void growFruit() {}
+//     public void dropLeaves() {}
+// }
