f71a98d0b8e398a28f6d3a80fce0c3abdfe32ad9
Donghyun An <miehdan@gmail.com>
2017-09-02 19:10:05 +0900

    Q290 - 99

---
+++ Explorer3.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Explorer3 {
+    public static void main(String[] args) {
+        TreeSet<Integer> s = new TreeSet<Integer>();
+        TreeSet<Integer> subs = new TreeSet<Integer>();
+        for(int i = 606; i < 613; i++)
+            if(i%2 == 0) 
+                s.add(i);
+        // System.out.println(s.subSet(608, true, 611, true));
+        subs = (TreeSet)s.subSet(608, true, 611, true);
+        subs.add(629);
+        System.out.println(s + " " + subs);
+    }
+}
