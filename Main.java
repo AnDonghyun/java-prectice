ab50f35a9a01b0aa386e02a168d506249d5ede14
Donghyun An <miehdan@gmail.com>
2017-09-02 19:52:59 +0900

    Q290 - 105

--- Main.java
+++ Main.java
@@ -1,14 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        Float pi = new Float(3.14f);
-        if (pi > 3) {
-            System.out.print("pi is bigger than 3. ");
-        }
-        else {
-            System.out.print("pi is not bigger than 3. ");
-        }
-        finally {
-            System.out.println("Have a nice day.");
+        try {
+            args = null;
+            args[0] = "test";
+            System.out.println(args[0]);
+        } catch (Exception ex) {
+            System.out.println("Exception");
+        } catch (NullPointerException npe) {
+            System.out.println("NullPointerException");
         }
     }
 }
