0353cd5ccb9ca525b7a5fdc2d5bd12aa9a7d4c2a
Donghyun An <miehdan@gmail.com>
2017-09-02 19:49:37 +0900

    Q290 - 104

--- Donkey.java
+++
@@ -1,9 +0,0 @@
-public class Donkey {
-    public static void main(String[] args) {
-        boolean assertsOn = false;
-        assert (assertsOn) : assertsOn = true;
-        if(assertsOn) {
-            System.out.println("assert is on");
-        }
-    }
-}
