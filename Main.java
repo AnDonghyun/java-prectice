a8df0c28c94f43fc5a2c942ce74ddb5d1c9f71f2
Donghyun An <miehdan@gmail.com>
2017-09-04 17:19:36 +0900

    Q290 - 146

--- Main.java
+++ Main.java
@@ -1,13 +1,16 @@
 public class Main {
-    public static void main(String[] args) throws Exception{
-        // throw new Exception();
-        try{
-            // throw new Exception();
-        } catch(Exception e){
-            throw new Exception();
-        } finally {
-            System.out.println("FINALLY");
+    static void test() {
+        try {
+            String x = null;
+            System.out.print(x.toString() + " ");
         }
+        catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        finally { System.out.print("finally "); }
     }
-    
+    public static void main(String[] args) {
+        try { test(); }
+        catch (Exception ex) { System.out.print("exception "); }
+    }
 }
