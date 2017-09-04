c854b4b0f22233bc8226fcd2dfd7a9942b5b7ca4
Donghyun An <miehdan@gmail.com>
2017-09-04 20:24:17 +0900

    Q290 - 148

--- Main.java
+++ Main.java
@@ -1,16 +1,15 @@
 public class Main {
-    static void test() {
-        try {
-            String x = null;
-            System.out.print(x.toString() + " ");
-        }
-        catch (Exception e) {
-            System.out.println(e.toString());
-        }
-        finally { System.out.print("finally "); }
-    }
-    public static void main(String[] args) {
-        try { test(); }
-        catch (Exception ex) { System.out.print("exception "); }
+    public static void main(String[] args){
+     try {
+        throw new NullPointerException();
+// some code here
+     } catch (NullPointerException e1) {
+        System.out.print("a");
+    } catch (Exception e2) {
+        System.out.print("b");
+    } finally {
+        System.out.print("c");
     }
 }
+}
+
